d12efecf42e54f9b9987ec0251472928b2804b81
xFijo <75216577+xFijo@users.noreply.github.com>
2021-07-22 21:22:04 +0100

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,11 @@
+namespace HelloWorld
+{
+    class Hello {         
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+// output: Hello World!
